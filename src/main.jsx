8b9f18533dbdf30451e93f2e88bbe05447081b33
Bscar <oscarosaurus@gmail.com>
2025-05-05 19:57:35 +0100

    added basic login functionality

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,21 @@
 // src/main.jsx
 
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router' // add import for BrowserRouter
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router';
+import { UserProvider } from './contexts/UserContext.jsx';
 
-// Wrap the App component with the BrowserRouter component to enable
-// enable route handling throughout your application.
+import App from './App.jsx';
+
+import './index.css';
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      {/* Wrap the UserProvider around the App */}
+      <UserProvider>
+        <App />
+      </UserProvider>
     </BrowserRouter>
   </StrictMode>,
-)
+);
